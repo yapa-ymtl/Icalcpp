8c4d24f8f52a70c7b725cc95e31e8b63774b383a
yapa <lakmaltharindu009@gmail.com>
2025-04-24 22:43:50 +0530

    Fix build issue in vevent

--- examples/simple.cpp
+++ examples/simple.cpp
@@ -1,8 +1,13 @@
+#include <iostream>
 #include "../include/icalcpp/icalcpp.h"
+// #include "icalcpp/vcalender/date.h"
 
 int main () {
 
-    Icalcpp::VCalender newObj = Icalcpp::VCalender("prod_id");
+    Icalcpp::VCalender newObj = Icalcpp::VCalender("prod_id", "3.0");
+    // std::cout << newObj.version();
+    std::cout << newObj;
+    // std::string method = newObj.version();
 
     return 0;
 }
